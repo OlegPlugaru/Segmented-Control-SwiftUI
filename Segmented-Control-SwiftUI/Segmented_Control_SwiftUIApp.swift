48c57b4b17674b4ae57af9aa783d868233fb7552
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-21 18:32:10 +0300

    Initial Commit

---
+++ Segmented-Control-SwiftUI/Segmented_Control_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Segmented_Control_SwiftUIApp.swift
+//  Segmented-Control-SwiftUI
+//
+//  Created by Whoami on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Segmented_Control_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
